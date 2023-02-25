d0a752d9c82bafe55f9434d5c1faa6e7628b84f0
Priti Roy <roypriti921@gmail.com>
2023-02-25 21:28:44 +0530

    added keplerData and model code

--- server/src/model/planets.model.js
+++ server/src/model/planets.model.js
@@ -1,5 +1,36 @@
-const planets = []
+const {parse} = require('csv-parse');
+const fs = require('fs');
+
+const exoplanet = [];
+
+let isHabitable =(planet)=>{
+    return planet['koi_disposition'] === 'CONFIRMED'
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.1
+    && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler_data.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true
+    }))
+    .on('data', (data) => {
+        if(isHabitable(data)){
+            exoplanet.push(data);
+        }
+    })
+    .on('error', (err) => {
+        console.log(err);
+    })
+    .on('end', () => {
+        console.log(exoplanet.map((planet)=>{
+            return planet['kepler_name'];
+        }))
+        console.log(`number of habitable planets found: ${exoplanet.length}`);
+        console.log('all done')
+    })
+
 
 module.exports = {
-    planets
+    planets: exoplanet
 }
