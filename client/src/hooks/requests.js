c58a3123d49513f7758f30f5f073b28b52d22187
Priti Roy <roypriti921@gmail.com>
2023-02-25 21:19:04 +0530

    added cors and whitelisted localhost:3000

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'v1';
+const API_URL = 'http://localhost:8000';
 
 // Load planets and return as JSON.
 async function httpGetPlanets() {
@@ -8,42 +8,42 @@ async function httpGetPlanets() {
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  const response = await fetch(`${API_URL}/launches`);
-  const fetchedLaunches = await response.json();
-  return fetchedLaunches.sort((a, b) => {
-    return a.flightNumber - b.flightNumber;
-  });
+  // const response = await fetch(`${API_URL}/launches`);
+  // const fetchedLaunches = await response.json();
+  // return fetchedLaunches.sort((a, b) => {
+  //   return a.flightNumber - b.flightNumber;
+  // });
 }
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  try {
-    return await fetch(`${API_URL}/launches`, {
-      method: "post",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(launch),
-    });
-  } catch(err) {
-    return {
-      ok: false,
-    };
-  }
+  // try {
+  //   return await fetch(`${API_URL}/launches`, {
+  //     method: "post",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(launch),
+  //   });
+  // } catch(err) {
+  //   return {
+  //     ok: false,
+  //   };
+  // }
 }
 
 // Delete launch with given ID.
 async function httpAbortLaunch(id) {
-  try {
-    return await fetch(`${API_URL}/launches/${id}`, {
-      method: "delete",
-    });
-  } catch(err) {
-    console.log(err);
-    return {
-      ok: false,
-    };
-  }
+  // try {
+  //   return await fetch(`${API_URL}/launches/${id}`, {
+  //     method: "delete",
+  //   });
+  // } catch(err) {
+  //   console.log(err);
+  //   return {
+  //     ok: false,
+  //   };
+  // }
 }
 
 export {
--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const PlanetsRouter = require("./router/planets/planets.router");
 
+
+app.use(cors({
+    options: 'http://localhost:3000'
+}))
 //parses incoming json data
 app.use(express.json());
 app.use(PlanetsRouter);
