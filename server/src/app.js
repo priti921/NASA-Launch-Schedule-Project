c3eb3c09cd4171414e82129ccfde1ed657e70f14
Priti Roy <roypriti921@gmail.com>
2023-03-18 14:54:39 +0530

    added comments

--- server/src/app.js
+++ server/src/app.js
@@ -7,21 +7,27 @@ const morgan = require("morgan");
 const PlanetsRouter = require("./router/planets/planets.router");
 const launchesRouter = require("./router/launches/launches.router");
 
+//cross origin access
 app.use(
   cors({
     options: "http://localhost:3000",
   })
 );
 
+//logging 
 app.use(morgan("combined"));
 
 //parses incoming json data
 app.use(express.json());
+//serves static frontend files
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+
+//routes
 app.use(PlanetsRouter);
 app.use(launchesRouter);
 
+//all routes serves index
 app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 });
--- server/src/model/launches.model.js
+++ server/src/model/launches.model.js
@@ -1,7 +1,10 @@
+//it's better to use new Map() instead of {}
 const launches = new Map();
 
+//flight id to keep track with
 let latestFlightNumber = 100;
 
+//demo data
 const launch = {
   flightNumber: 100,
   mission: "Kepler Exploration X",
@@ -13,32 +16,47 @@ const launch = {
   success: true,
 };
 
+//checks if the launch id exists
 function existsLaunchById(id) {
   return launches.has(id);
 }
 
+//getting all launches from this model
 function getAllLaunches() {
+
+  //creates an array from launches object values
   return Array.from(launches.values());
 }
 
+//adding new launches with some iterable data added before hand
 function addNewLaunch(launchData) {
+  //adds new id
   latestFlightNumber++;
+
+  //then sets a new element with key and value to the launches map
+  //in the value i create object and spread the data 
   launches.set(latestFlightNumber, {
     ...launchData,
-    flightNumber: latestFlightNumber,
+    flightNumber: latestFlightNumber, //adding the incremented flight number
     customer: ["ISRO", "NASA"],
     upcoming: true,
     success: true,
   });
 }
 
+//aborting launch by changing their success and upcoming to false
 function abortLaunchById(id) {
   const abort = launches.get(id);
   abort.success = false;
   abort.upcoming = false;
 }
 
+
+//the launch model gets added to the launches map with a key and a value
 launches.set(launch.flightNumber, launch);
+
+console.log(launches);
+
 module.exports = {
   getAllLaunches,
   addNewLaunch,
