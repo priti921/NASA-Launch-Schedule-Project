c1b9b4d4b0559993e789d091123ee4d5ab2dccae
Priti Roy <roypriti921@gmail.com>
2023-03-21 11:44:44 +0530

    connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,15 +1,23 @@
+require('dotenv').config() //need to import dotenv to use env variables
 //so it doesn't conflict with the client port
 //checks if the port is already in use
 const app = require("./app");
 const http = require("http");
-const { loadPlanets } = require("./model/planets.model");
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
+
+const { loadPlanets } = require("./model/planets.model");
+
 const mongoose = require('mongoose')
+const username = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+
+
+
 
 
 //connecting mongodb
-const MONGO_URL = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@nasacluster.f4iu3on.mongodb.net/?retryWrites=true&w=majority`
+const MONGO_URL = `mongodb+srv://${username}:${password}@nasacluster.f4iu3on.mongodb.net/?retryWrites=true&w=majority`
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!')
